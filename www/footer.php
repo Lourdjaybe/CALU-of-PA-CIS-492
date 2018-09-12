9bd1ecb867653b90d514dd5baa22dbb89e565301
BealeMJ <43020531+BealeMJ@users.noreply.github.com>
2018-09-12 01:43:52 -0400

    Add files via upload

--- www/footer.php
+++ www/footer.php
@@ -3,7 +3,7 @@
       <footer>
         <p> California University of Pennsylvania, 250 University Ave.,
           California, PA 15419 <br>
-          ? Copyright 2018 California University of Pennsylvania </p>
+          Copyright 2018 California University of Pennsylvania </p>
       </footer>
     </center>
   </body>
